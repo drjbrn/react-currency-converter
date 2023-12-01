c3b26d595dc51935391ecc82039182d89216b2d5
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-12-01 17:09:05 +0200

    feat: add App component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from './components/Header';
+import { CurrencyConverter } from './components/CurrencyConverter';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <CurrencyConverter />
+    </>
+  );
+};
